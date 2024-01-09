426359af260854203875b631626b5e04dd9a8dc0
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-01-09 11:21:13 -0300

    Fim da Aula 10

--- lib/src/modules/home/repositories/implementations/chat_repository.dart
+++ lib/src/modules/home/repositories/implementations/chat_repository.dart
@@ -14,38 +14,24 @@ class ChatRepository implements IChatRepository {
     String userID,
     List<ContactModel> contacts,
   ) async {
-    final filterMyID = FilterParam.contains('contacts', userID);
-    final filterPrivateChat = FilterParam.equal('type', 'private');
-
-    final chats = await _apiService.get(
-      'chats',
-      filters: [filterMyID, filterPrivateChat],
-    );
+    final chats = await _getChat(currentUserID: userID, isPrivateChat: true);
 
     final models = <ChatModel>[];
 
     for (final chat in chats) {
-      final chatUsers = chat['users_id'] as List;
+      final chatID = chat['id'];
+      final chatUsers = List<String>.from(chat['users_id']);
       final otherUserID = chatUsers.firstWhere((e) => e != userID);
       final contact = contacts.firstWhere((e) => e.id == otherUserID);
 
-      final currentChatFilter = FilterParam.equal('chat_id', chat['id']);
-      final lastMessages = await _apiService.get(
-        'messages',
-        filters: [currentChatFilter],
-        orderBy: const OrderByParam(key: 'sended_at'),
-      );
-      final lastMessage = lastMessages.first;
+      final lastMessage = await _getLastMessageByChatID(chatID);
+      final unreadMessagesCount = await _getUnreadePrivateMessagesAmount(
+          chatID: chatID, otherUserID: otherUserID);
 
       // TODO: Conectar com Firebase
-      final unrealFilter = FilterParam.equal('unreal', true);
-      final unreadMessagesCount = await _apiService.count(
-        'messages',
-        filters: [currentChatFilter, unrealFilter],
-      );
 
       final data = {
-        'id': chat['id'],
+        'id': chatID,
         'contacts': chat['users_id'],
         'chat_status': chat['chat_status'],
         'image_url': contact.imageUrl,
@@ -56,15 +42,97 @@ class ChatRepository implements IChatRepository {
         'unread_messages_count': unreadMessagesCount,
       };
 
-      final model = ChatModel.fromMap(data);
-      models.add(model);
+      models.add(ChatModel.fromMap(data));
     }
     return models;
   }
 
   @override
-  Future<List<ChatModel>> getGroupChats(String userID) {
-    // TODO: implement getGroupChats
-    throw UnimplementedError();
+  Future<List<ChatModel>> getGroupChats(String userID) async {
+    final chats = await _getChat(currentUserID: userID, isPrivateChat: false);
+
+    final models = <ChatModel>[];
+
+    for (final chat in chats) {
+      final chatID = chat['id'];
+
+      final lastMessage = await _getLastMessageByChatID(chatID);
+      final unreadMessagesCount = await _getUnreadeGroupMessagesAmount(
+        chatID: chatID,
+        currentUserID: userID,
+      );
+
+      // TODO: Conectar com Firebase
+
+      final data = {
+        'id': chatID,
+        'contacts': chat['users_id'],
+        'chat_status': chat['chat_status'],
+        'image_url': chat['image_url'],
+        'name': chat['name'],
+        'user_status': null,
+        'message_date': lastMessage['sended_at'],
+        'last_message': lastMessage['contact'],
+        'unread_messages_count': unreadMessagesCount,
+      };
+
+      models.add(ChatModel.fromMap(data));
+    }
+
+    return models;
+  }
+
+  Future<List<Map<String, dynamic>>> _getChat({
+    required String currentUserID,
+    required bool isPrivateChat,
+  }) async {
+    final currrentUserFilter = FilterParam.contains('contacts', currentUserID);
+    final chatType = isPrivateChat ? 'private' : 'group';
+    final chatFilterType = FilterParam.equal('type', chatType);
+
+    final chatData = await _apiService
+        .get('chats', filters: [currrentUserFilter, chatFilterType]);
+
+    return chatData;
+  }
+
+  Future<Map<String, dynamic>> _getLastMessageByChatID(String chatID) async {
+    final currentChatFilter = FilterParam.equal('chat_id', chatID);
+    final lastMessages = await _apiService.get(
+      'messages',
+      filters: [currentChatFilter],
+      orderBy: const OrderByParam(key: 'sended_at'),
+      limit: 1,
+    );
+    final lastMessage = lastMessages.first;
+
+    return lastMessage;
+  }
+
+  Future<int> _getUnreadePrivateMessagesAmount({
+    required chatID,
+    required otherUserID,
+  }) async {
+    final currentChatFilter = FilterParam.equal('chat_id', chatID);
+    final unrealFilter = FilterParam.equal('viewed', false);
+    final senderFilter = FilterParam.equal('sender', otherUserID);
+    final unreadMessagesCount = await _apiService.count(
+      'messages',
+      filters: [currentChatFilter, unrealFilter, senderFilter],
+    );
+
+    return unreadMessagesCount;
+  }
+
+  Future<int> _getUnreadeGroupMessagesAmount(
+      {required chatID, required currentUserID}) async {
+    final currentChatFilter = FilterParam.equal('chat_id', chatID);
+    final unrealFilter = FilterParam.contains('not_viewed_by', currentUserID);
+    final unreadMessagesCount = await _apiService.count(
+      'messages',
+      filters: [currentChatFilter, unrealFilter],
+    );
+
+    return unreadMessagesCount;
   }
 }
